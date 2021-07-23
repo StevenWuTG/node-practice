06a95a796736f439b5e8301b2dc4e81477411787
StevenWuTG <stevenwu91@icloud.com>
2021-07-23 14:53:34 -0400

    express middleware assignment done

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,8 +1,22 @@
-const http = require('http');
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+app.use('/users',(req, res, next) => {
+    console.log("in middleware users")
+    res.send("<h1> /users middleware</h1>")
+    // next()
+
+})
+
+app.use('/', (req, res, next) => {
+    console.log('/ middleware')
+    res.send('<h1> / middleware</h1>')
+})
 
 
-console.log(routes.someText);
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3000);
