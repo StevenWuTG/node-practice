27ccd6e23dca68ac21003fd6cd76c314fe0e137e
StevenWuTG <stevenwu91@icloud.com>
2021-07-29 11:55:36 -0400

    pug exercise done

--- node-routes/routes/shop.js
+++ node-routes/routes/shop.js
@@ -7,9 +7,10 @@ const adminData = require('./admin')
 
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
-  console.log('in shop.js',adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products
+  res.render('shop', {prods: products, docTitle: 'Shop'})
 });
 
 module.exports = router;
