ce0ccf2bd26363d0ff831dc403f7269593645899
StevenWuTG <stevenwu91@icloud.com>
2021-08-04 11:14:26 -0400

    connected db

--- node-mongoAtlas/util/database.js
+++ node-mongoAtlas/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://bockchoy:tP4life@cluster0.r9tdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('connected')
+    callback(client)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+}
+
+module.exports = mongoConnect
