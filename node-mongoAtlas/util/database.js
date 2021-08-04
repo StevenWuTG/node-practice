eaae5cba62d44ee1aa6b35ceb5ce01652c9502fa
StevenWuTG <stevenwu91@icloud.com>
2021-08-04 12:18:44 -0400

    post to mongo sucess

--- node-mongoAtlas/util/database.js
+++ node-mongoAtlas/util/database.js
@@ -1,17 +1,30 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
+let _ 
+
 
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://bockchoy:tP4life@cluster0.r9tdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  
+  MongoClient.connect('mongodb+srv://bockchoy:tP4life@cluster0.r9tdw.mongodb.net/shop?retryWrites=true&w=majority')
   .then(client => {
     console.log('connected')
-    callback(client)
+    _db = client.db()
+    callback()
   })
   .catch(err => {
     console.log(err)
+    throw err
   })
 
 }
 
-module.exports = mongoConnect
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw "No DB found!"
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
